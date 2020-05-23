89e3880160c4361a7269e095335358df247306c2
OLEGSHA <kvadropups@gmail.com>
2020-05-23 16:14:04 +0300

    Screw that, let's make this into a proper fork

--- src/main/java/twilightforest/TwilightForestMod.java
+++ src/main/java/twilightforest/TwilightForestMod.java
@@ -59,18 +59,11 @@ import thaumcraft.api.*;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
-@Mod(
-		modid = TwilightForestMod.ID,
-		name = "The Twilight Forest",
-		version = TwilightForestMod.VERSION,
-		
-		// Unmodified version accepted
-		acceptableRemoteVersions = "2.3.7"
-)
+@Mod(modid = TwilightForestMod.ID, name = "The Twilight Forest", version = TwilightForestMod.VERSION)
 public class TwilightForestMod {
 	
 	public static final String ID = "TwilightForest";
-	public static final String VERSION = "2.3.8dev-PIWCS1";
+	public static final String VERSION = "2.3.8dev-PIWCS2";
 	
 	public static final String MODEL_DIR = "twilightforest:textures/model/";
 	public static final String GUI_DIR = "twilightforest:textures/gui/";
